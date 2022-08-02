00e5061677aa2013793abd22bb2292ca6566da29
Roberto Carlos <betocarlos00@hotmail.com>
2022-08-01 22:14:57 -0300

    update controller - multiplos channels

--- estudos/WebApiRabbitMQ/Controllers/OrderController.cs
+++ estudos/WebApiRabbitMQ/Controllers/OrderController.cs
@@ -5,6 +5,8 @@ using RabbitMQ.Client;
 using System;
 using System.Text;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 using WebApiRabbitMQ.Domain;
 
 namespace WebApiRabbitMQ.Controllers
@@ -20,37 +22,59 @@ namespace WebApiRabbitMQ.Controllers
             _looger = looger;
         }
 
-        [HttpPost]
-        public IActionResult InsertOrder(Order order)
+        [HttpGet]
+        public IActionResult InsertOrder()
         {
-            try
+            Publisher();
+
+            return Ok();
+        }
+
+        private void Publisher()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
             {
-                var factory = new ConnectionFactory() { HostName = "localhost" };
-                using (var connection = factory.CreateConnection())
-                using (var channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare(queue: "orderQueue",
+                var queueName = "order";
+
+                var channel1 = CreateChannel(connection);
+                var channel2 = CreateChannel(connection);
+
+                BuildPublishers(channel1, queueName, "Produtor A");
+                BuildPublishers(channel2, queueName, "Produtor B");
+            }
+        }
+
+        // -- services
+        private IModel CreateChannel(IConnection connection)
+        {
+            var channel = connection.CreateModel();
+
+            return channel;
+        }
+
+        private void BuildPublishers(IModel channel, string queue, string publisherName)
+        {
+            Task.Run(() =>
+            {
+                int count = 0;
+
+                channel.QueueDeclare(queue: "orderQueue",
                                          durable: false,
                                          exclusive: false,
                                          autoDelete: false,
                                          arguments: null);
 
-                    string message = JsonSerializer.Serialize(order);
+                while (true)
+                {
+                    string message = $"OrderNumber: {count++}";
                     var body = Encoding.UTF8.GetBytes(message);
 
-                    channel.BasicPublish(exchange: "",
-                                         routingKey: "orderQueue",
-                                         basicProperties: null,
-                                         body: body);
-                }
+                    channel.BasicPublish("", queue, null, body);
 
-                return Accepted(order);
-            }
-            catch (Exception ex)
-            {
-                _looger.LogError("Erro ao tentar criar um novo pedido", ex);
-                return new StatusCodeResult(500);
-            }
+                    Thread.Sleep(1000);
+                }
+            });
         }
     }
 }
